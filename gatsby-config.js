98bfcf9efe0a1750cdcf0c0cf05423514a9cc4aa
Luke Herrington <lherrington100@gmail.com>
2019-05-22 20:28:34 +0100

    fix manifest info

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,17 +34,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-default-mdx-basic',
-        short_name: 'starter',
+        name: 'Luke Herrington\'s Site',
+        short_name: 'Luke\'s Website',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: 'white',
+        theme_color: '#00CCFF',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: './src/images/luke-square-sm.png', // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    'gatsby-plugin-offline',
   ],
 }
