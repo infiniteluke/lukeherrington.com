62490c5d89423acc5fea3d3dd0898550c829705b
Luke Herrington <lherrington100@gmail.com>
2019-05-22 23:15:37 +0100

    fix viewport with custom html.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
-import Helmet from 'react-helmet';
 
 import Header from './header'
 import './layout.css'
@@ -22,9 +21,6 @@ const Layout = ({ children }) => (
       <main style={{
         padding: 'env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left)'
       }}>
-        <Helmet>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
-        </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
           style={{
