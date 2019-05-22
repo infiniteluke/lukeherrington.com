36bac197286226f2b6b9a8b657dfc22b161b44b0
Luke Herrington <lherrington100@gmail.com>
2019-05-22 23:08:17 +0100

    fix iOS Safari issues

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
+import Helmet from 'react-helmet';
 
 import Header from './header'
 import './layout.css'
@@ -18,7 +19,12 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
+      <main style={{
+        padding: 'env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left)'
+      }}>
+        <Helmet>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+        </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
           style={{
@@ -30,7 +36,7 @@ const Layout = ({ children }) => (
         >
           {children}
         </div>
-      </>
+      </main>
     )}
   />
 )
