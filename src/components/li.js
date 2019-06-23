b3d57386827043f1bc2c23e39f2da09694f561c4
Luke Herrington <lherrington100@gmail.com>
2019-06-23 13:57:05 -0700

    improve a11y

--- src/components/li.js
+++ src/components/li.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Label from './label';
 
-export default ({ link, children, label }) => (
-  <li>{link?(<a style={{ verticalAlign: 'top' }} href={link}>{label?(<Label>{label}</Label>):null}{children}</a>) : label?(<><Label>{label}</Label>{children}</>):children}</li>
+export default ({ children, label }) => (
+  <li>{label?(<Label>{label}</Label>):null}{children}</li>
 )
