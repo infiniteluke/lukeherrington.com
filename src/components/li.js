a8e986cf4bfad60eabf37cc9eb099ab60477d21c
Luke Herrington <lherrington100@gmail.com>
2019-06-23 09:26:38 -0700

    top align text in list items

--- src/components/li.js
+++ src/components/li.js
@@ -2,5 +2,5 @@ import React from 'react';
 import Label from './label';
 
 export default ({ link, children, label }) => (
-  <li>{link?(<a href={link}>{label?(<Label>{label}</Label>):null}{children}</a>) : label?(<><Label>{label}</Label>{children}</>):children}</li>
+  <li>{link?(<a style={{ verticalAlign: 'top' }} href={link}>{label?(<Label>{label}</Label>):null}{children}</a>) : label?(<><Label>{label}</Label>{children}</>):children}</li>
 )
