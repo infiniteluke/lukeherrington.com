0cff550777566315fd018f590d0fecaed8e403cc
Luke Herrington <luke@apella.io>
2024-11-05 15:30:35 -0800

    Start over

--- src/components/li.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Label from './label';
-
-export default ({ children, label }) => (
-  <li className="list-item">
-    {label ? <Label>{label}</Label> : null}
-    {children}
-  </li>
-);
