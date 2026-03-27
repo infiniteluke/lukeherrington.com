ae85aef2f6c7ec4ff56c843ebd32d390f78cd679
Luke Herrington <luke@apella.io>
2026-03-27 16:54:52 -0700

    Simplify to static site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()]
   }
